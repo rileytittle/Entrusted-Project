5671b2b9d92a2038d022f318b36979906388348c
rileytittle <coletittle@ymail.com>
2025-03-26 16:58:40 -0400

    create basic task view

--- frontend/src/pages/TasksPage.jsx
+++ frontend/src/pages/TasksPage.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 function TasksPage() {
-	return <div>TasksPage</div>;
+	const [tasks, setTasks] = useState([]);
+	useEffect(() => {
+		axios
+			.get("http://localhost:3000/tasks")
+			.then((res) => {
+				setTasks(res.rows);
+			})
+			.catch((error) => {
+				//handle the error
+			});
+	});
+	return (
+		<div>
+			{tasks.map((task) => (
+				<p>
+					{task.id}, {task.name}, {task.done}
+				</p>
+			))}
+		</div>
+	);
 }
 
 export default TasksPage;
