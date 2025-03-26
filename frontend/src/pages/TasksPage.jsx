52443ed9df57b16b72aaac0aa79dc052b369fc4d
rileytittle <coletittle@ymail.com>
2025-03-26 17:54:44 -0400

    get bootstrap modal working

--- frontend/src/pages/TasksPage.jsx
+++ frontend/src/pages/TasksPage.jsx
@@ -4,6 +4,16 @@ import axios from "axios";
 
 function TasksPage() {
 	const [tasks, setTasks] = useState([]);
+	const [modalVisible, setModalVisible] = useState(false);
+	const [newTaskName, setNewTaskName] = useState("");
+
+	function createTask() {
+		if (newTaskName) {
+			console.log("this will create a task with name:", newTaskName);
+			setNewTaskName("");
+		}
+		setModalVisible(false);
+	}
 	useEffect(() => {
 		axios
 			.get("http://localhost:3000/tasks")
@@ -15,27 +25,74 @@ function TasksPage() {
 			});
 	});
 	return (
-		<div className="card">
-			<div className="card-body">
-				<h5 className="card-title">Tasks</h5>
-				{tasks.length > 0 ? (
-					<div>
-						{tasks.map((task) => (
-							<div className="card mb-3" key={task.id}>
-								<div className="card-body">
-									{task.id}, {task.name}, {task.done}
+		<>
+			<div className="card">
+				<div className="card-body">
+					<h5 className="card-title">Tasks</h5>
+					{tasks.length > 0 ? (
+						<div>
+							{tasks.map((task) => (
+								<div className="card mb-3" key={task.id}>
+									<div className="card-body">
+										{task.id}, {task.name}, {task.done}
+									</div>
 								</div>
-							</div>
-						))}
-					</div>
-				) : (
-					<p>Tasks loading</p>
-				)}
-				<a href="#" class="btn btn-primary">
-					Add another
-				</a>
+							))}
+						</div>
+					) : (
+						<p>Tasks loading</p>
+					)}
+					<button
+						className="btn btn-primary"
+						onClick={() => setModalVisible(true)}
+					>
+						Add another
+					</button>
+				</div>
 			</div>
-		</div>
+			{modalVisible && (
+				<div className="modal fade show" style={{ display: "block" }}>
+					<div className="modal-dialog">
+						<div className="modal-content">
+							<div className="modal-header">
+								<h5 className="modal-title">Create New Task</h5>
+							</div>
+							<div className="modal-body">
+								<label htmlFor="name">Task name</label>
+								<input
+									name="name"
+									id="name"
+									type="text"
+									value={newTaskName}
+									onChange={(e) =>
+										setNewTaskName(e.target.value)
+									}
+								/>
+							</div>
+							<div className="modal-footer">
+								<button
+									type="button"
+									className="btn btn-secondary"
+									data-bs-dismiss="modal"
+									onClick={() => {
+										setModalVisible(false);
+									}}
+								>
+									Close
+								</button>
+								<button
+									type="button"
+									className="btn btn-primary"
+									onClick={createTask}
+								>
+									Save changes
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+		</>
 	);
 }
 
