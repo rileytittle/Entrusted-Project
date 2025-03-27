6e7699873deb32abdb27c6b76ef6a2ac643908d2
rileytittle <coletittle@ymail.com>
2025-03-26 20:08:27 -0400

    update class to className for React

--- frontend/src/pages/TasksPage.jsx
+++ frontend/src/pages/TasksPage.jsx
@@ -86,9 +86,11 @@ function TasksPage() {
 							</button>
 						</div>
 					) : (
-						<div class="d-flex justify-content-center">
-							<div class="spinner-border" role="status">
-								<span class="visually-hidden">Loading...</span>
+						<div className="d-flex justify-content-center">
+							<div className="spinner-border" role="status">
+								<span className="visually-hidden">
+									Loading...
+								</span>
 							</div>
 						</div>
 					)}
